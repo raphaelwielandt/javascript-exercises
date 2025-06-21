d43937ffdf4e62e0da44534a767ca803e84209d5
Raphael Wielandt <170776378+raphaelwielandt@users.noreply.github.com>
2025-06-21 17:42:07 +0200

    Finish exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let celcius = (temp - 32) * (5 / 9);
+  return Math.round(celcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(32));
+
+const convertToFahrenheit = function (temp) {
+  let fahrenheit = temp * (9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };

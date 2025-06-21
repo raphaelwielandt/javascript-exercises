84d66cce89677446721925e4bd898d7fadae892c
Raphael Wielandt <170776378+raphaelwielandt@users.noreply.github.com>
2025-06-21 17:05:50 +0200

    Finish exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
 
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
+  if ((a || b) < 0) return "ERROR";
+
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
+
+sumAll(2, 4);
 
 // Do not edit below this line
 module.exports = sumAll;

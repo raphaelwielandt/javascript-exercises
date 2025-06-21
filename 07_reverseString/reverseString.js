ebe7923f2ad60d8e6efb07262a1ceb359d0e3269
Raphael Wielandt <170776378+raphaelwielandt@users.noreply.github.com>
2025-06-21 16:13:36 +0200

    Finish exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  if (typeof string !== "string") throw new TypeError();
 
+  let charArray = string.split("");
+
+  return charArray.reverse().join("");
 };
+
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;

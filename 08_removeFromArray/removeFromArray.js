b3a236e053b042a3b71dbdf99a2b6c28286303dc
Raphael Wielandt <170776378+raphaelwielandt@users.noreply.github.com>
2025-06-21 16:43:00 +0200

    Finish exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+/**
+ *
+ * @param {any[]} arr
+ * @param {number} number
+ */
+const removeFromArray = function (arr, ...args) {
+  let arrCopy = [...arr];
+
+  return arrCopy.filter((item) => !args.includes(item));
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;

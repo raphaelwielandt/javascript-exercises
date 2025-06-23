4b9ce0b14116e4c2cc9551a8f29a0c648cd6e15a
Raphael Wielandt <170776378+raphaelwielandt@users.noreply.github.com>
2025-06-23 22:01:29 +0200

    Finish exercise 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = parseInt(n);
 
+  if (n < 0) return 'OOPS';
+
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+  for (let i = 2; i <= n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return b;
 };
 
 // Do not edit below this line

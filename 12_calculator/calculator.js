50acd988264d2e23dc6890200d395661d725591b
Raphael Wielandt <170776378+raphaelwielandt@users.noreply.github.com>
2025-06-23 21:06:16 +0200

    Finish exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+/**
+ *
+ * @param {Array} numbers
+ */
+const sum = function (numbers) {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+/**
+ *
+ * @param {Array} numbers
+ */
+const multiply = function (numbers) {
+  return numbers.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num < 0) return;
+  else if (num === 0) return 1;
+  else {
+    let fact = 1;
+    for (i = 1; i <= num; i++) {
+      fact *= i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };

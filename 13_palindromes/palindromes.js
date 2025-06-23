59bbe2d547fa8822e6605fb54b70836ce7ab8c42
Raphael Wielandt <170776378+raphaelwielandt@users.noreply.github.com>
2025-06-23 21:38:32 +0200

    Finish exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+/**
+ *
+ * @param {string} string
+ */
+const palindromes = function (string) {
+  let arr = [...string]
+    .filter((char) => /[a-zA-Z0-9]/.test(char)) //Regex to check for letters and numbers
+    .join('')
+    .toLowerCase();
 
+  let arr1 = [...arr].reverse().join('');
+
+  return arr === arr1;
 };
 
 // Do not edit below this line
